59fe170a7270400b1539a359570106fcfb0d058c
KhalilJouaneh <khalil_jouaneh@yahoo.com>
2022-07-27 16:32:25 +0300

    fixed import error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { NFTCard } from '../components/nftCard'
 
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
